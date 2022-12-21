10bf8ab4e4f0b186741df03ebb34120a2ce3c33b
MCHN <wiktormachon@o2.pl>
2022-12-21 19:33:39 +0100

    fixed login error, changed login view appeareance :pizza:

--- desk_booking/app/urls.py
+++ desk_booking/app/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path("accounts/login/", views.login, name='login'),  
+    path("login/", views.login, name='login'),  
 
 ]
--- desk_booking/app/views.py
+++ desk_booking/app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .forms import SignUpForm
 #from django.template import loader
 
 
@@ -12,8 +13,11 @@ def index(request):
     return HttpResponse(template.render(context, request))
 
 def login(request):
-    context = {
-        'test': list(range(0,5)),
-    }
-    template = loader.get_template('desk_booking/login.html')
-    return HttpResponse(template.render(context, request))
+    
+    # context = {
+    #     'test': list(range(0,5)),
+    # }
+    # template = loader.get_template('desk_booking/login.html')
+    
+    form = SignUpForm()
+    return render(request, 'desk_booking/login.html', {'form': form})
